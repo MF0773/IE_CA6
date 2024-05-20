cddd492cc5a7f877c464a8581571650f9a7c335e
m.fateh <mohammad.fateh256@ut.ac.ir>
2024-05-20 16:20:03 +0330

    feat: adding the auth chain

--- src/main/java/org/ie/mizdooni/config/CorsConfig.java
+++ src/main/java/org/ie/mizdooni/config/CorsConfig.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Arrays;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig {
     private static final Long MAX_AGE = 3600L;
     private static final int CORS_FILTER_ORDER = -102;
@@ -18,24 +20,16 @@ public class CorsConfig {
     public FilterRegistrationBean<CorsFilter> corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-
-        // Allow only localhost origin
         config.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
-
-        // Allowed HTTP methods
         config.setAllowedMethods(Arrays.asList("POST", "PUT", "PATCH", "DELETE", "GET"));
-
-        // Allowed headers
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
 
-        // Max age for preflight request
         config.setMaxAge(MAX_AGE); // 30 minutes in seconds
 
-        // Apply CORS configuration on every path
         source.registerCorsConfiguration("/**", config);
 
         FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(CORS_FILTER_ORDER); // Set the highest precedence to ensure it runs first
+        bean.setOrder(CORS_FILTER_ORDER);
         return bean;
     }
 }
--- src/main/java/org/ie/mizdooni/model/UserModel.java
+++ src/main/java/org/ie/mizdooni/model/UserModel.java
@@ -12,66 +12,94 @@ import java.util.stream.Collectors;
 @Entity
 @Table(name = "Users")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="role",
-        discriminatorType = DiscriminatorType.STRING)
+@DiscriminatorColumn(name = "role", discriminatorType = DiscriminatorType.STRING)
 public class UserModel extends BaseModel {
-//    static private List<UserModel> allObjects = new ArrayList<>();
+    // static private List<UserModel> allObjects = new ArrayList<>();
 
-//    public enum UserRole {
-//        @JsonProperty("client")
-//        CLIENT,
-//        @JsonProperty("manager")
-//        MANAGER
-//    };
+    // public enum UserRole {
+    // @JsonProperty("client")
+    // CLIENT,
+    // @JsonProperty("manager")
+    // MANAGER
+    // };
 
-//    public UserRole getRole() {
-//        return role;
-//    }
-//
-//    public void setRole(UserRole role) {
-//        this.role = role;
-//    }
+    // public UserRole getRole() {
+    // return role;
+    // }
+    //
+    // public void setRole(UserRole role) {
+    // this.role = role;
+    // }
 
     @Id
     @FieldValidator(UserNameVlidator.class)
     protected String username;
     protected String password;
-//    @Column(unique = true)
+    protected String token;
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    // @Column(unique = true)
     @FieldValidator(EmailValidator.class)
     protected String email;
     protected String country;
     protected String city;
 
-    public UserModel() {}
+    public UserModel() {
+    }
+
     public String getUsername() {
         return username;
     }
-    public void setUsername(String username) { this.username = username;}
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getCountry() {
         return country;
     }
+
     public void setCountry(String country) {
         this.country = country;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
-    public String getRole() {return null;};
+
+    public String getRole() {
+        return null;
+    };
+
+    public String getLogin() {
+        return getUsername() + getPassword();
+    } // TODO change it into a external record
 
     // @Override public void validate() throws ValidatorException{
     // var usernameValidator = new UserNameVlidator();
@@ -79,38 +107,40 @@ public class UserModel extends BaseModel {
     // (new EmailValidator() ).validate(this.email);
     // }
 
-//    static private UserModel loginnedUser = null;
-//
-//    public static UserModel getLoginnedUser() {
-//        return loginnedUser;
-//    }
-//
-//    public static void setLoginnedUser(UserModel user) {
-//        loginnedUser = user;
-//    }
-//
-//    static public List<UserModel> findUserByUserPass(String username, String password) {
-//        List<UserModel> matchedUserList = allObjects.stream().filter(
-//                user -> user.getUsername().compareTo(username) == 0 && user.getPassword().compareTo(password) == 0)
-//                .collect(Collectors.toList());
-//        return matchedUserList;
-//    }
-//
-//    static public void addObject(UserModel user) {
-//        allObjects.add(user); // TODO : remove data redundancy
-//    }
-//
-//    static public List<UserModel> getAllObjects() {
-//        return allObjects.stream().toList();
-//    }
-//
-//    static public UserModel findByUsername(String username) {
-//        List<UserModel> matchedUserList = allObjects.stream().filter(
-//                user -> user.getUsername().compareTo(username) == 0).collect(Collectors.toList());
-//        if (matchedUserList.isEmpty()) {
-//            return null;
-//        }
-//        return matchedUserList.get(0);
-//    }
+    // static private UserModel loginnedUser = null;
+    //
+    // public static UserModel getLoginnedUser() {
+    // return loginnedUser;
+    // }
+    //
+    // public static void setLoginnedUser(UserModel user) {
+    // loginnedUser = user;
+    // }
+    //
+    // static public List<UserModel> findUserByUserPass(String username, String
+    // password) {
+    // List<UserModel> matchedUserList = allObjects.stream().filter(
+    // user -> user.getUsername().compareTo(username) == 0 &&
+    // user.getPassword().compareTo(password) == 0)
+    // .collect(Collectors.toList());
+    // return matchedUserList;
+    // }
+    //
+    // static public void addObject(UserModel user) {
+    // allObjects.add(user); // TODO : remove data redundancy
+    // }
+    //
+    // static public List<UserModel> getAllObjects() {
+    // return allObjects.stream().toList();
+    // }
+    //
+    // static public UserModel findByUsername(String username) {
+    // List<UserModel> matchedUserList = allObjects.stream().filter(
+    // user -> user.getUsername().compareTo(username) ==
+    // 0).collect(Collectors.toList());
+    // if (matchedUserList.isEmpty()) {
+    // return null;
+    // }
+    // return matchedUserList.get(0);
+    // }
 }
-
